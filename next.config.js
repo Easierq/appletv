0edd7eb148394bbe66c6c06140bce708fe618653
Easierq <isiaqofficial@gmail.com>
2025-02-11 18:19:10 +0100

    new updates

--- next.config.js
+++ next.config.js
@@ -9,6 +9,11 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   webpack: (config) => {
     config.externals.push({
       "utf-8-validate": "commonjs utf-8-validate",
